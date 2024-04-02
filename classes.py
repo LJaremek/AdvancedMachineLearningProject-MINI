9b44dcfd2cc1bac6d1b31491699e2df2867ed684
Patrycja Wysocka <w512patrycja@gmail.com>
2024-04-02 12:05:33 +0200

    added stopping rule

--- classes.py
+++ classes.py
@@ -18,6 +18,7 @@ class BaseLogisticRegression:
             terms between features in the model.
 
     """
+
     def __init__(self, include_interactions: bool = False) -> None:
         """
         Initializes the BaseLogisticRegression class.
@@ -45,7 +46,7 @@ class BaseLogisticRegression:
         n = X.shape[1]
         interaction_terms = np.array(
             [X[:, i] * X[:, j] for i, j in combinations(range(n), 2)]
-            ).T
+        ).T
 
         return np.hstack([X, interaction_terms])
 
@@ -81,9 +82,7 @@ class BaseLogisticRegression:
             NotImplementedError: Indicates that the method needs to be
                 implemented by subclasses.
         """
-        raise NotImplementedError(
-            "This method should be implemented by subclass."
-            )
+        raise NotImplementedError("This method should be implemented by subclass.")
 
     def predict_proba(self, X: np.array) -> float:
         """
@@ -135,15 +134,17 @@ class ADAMLogisticRegression(BaseLogisticRegression):
         beta2: float - exponential decay rate for the second moment estimates
         epsilon: float - small number to prevent any division by zero
     """
+
     def __init__(
-            self,
-            learning_rate: float = 0.001,
-            iterations: int = 1000,
-            beta1: float = 0.9,
-            beta2: float = 0.999,
-            epsilon: float = 1e-8,
-            include_interactions: bool = False
-            ) -> None:
+        self,
+        learning_rate: float = 0.001,
+        iterations: int = 1000,
+        beta1: float = 0.9,
+        beta2: float = 0.999,
+        epsilon: float = 1e-8,
+        threshold: float = 1e-5,
+        include_interactions: bool = False,
+    ) -> None:
         """
         Initializes the ADAMLogisticRegression class with specified parameters.
 
@@ -162,6 +163,7 @@ class ADAMLogisticRegression(BaseLogisticRegression):
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.threshold = threshold
 
     def fit(self, X: np.array, y: np.array) -> None:
         """
@@ -177,20 +179,33 @@ class ADAMLogisticRegression(BaseLogisticRegression):
         m = np.zeros(X.shape[1])
         v = np.zeros(X.shape[1])
 
+        prev_log_likelihood = -1_000_000  # Initialize with a large negative value
+
         for t in range(1, self.iterations + 1):
             model = np.dot(X, self.weights)
             predictions = sigmoid(model)
             errors = y - predictions
             gradient = -np.dot(X.T, errors) / len(X)
 
-            m = self.beta1 * m + (1 - self.beta1) * gradient
-            v = self.beta2 * v + (1 - self.beta2) * (gradient ** 2)
-            m_hat = m / (1 - self.beta1 ** t)
-            v_hat = v / (1 - self.beta2 ** t)
-
-            self.weights -= (
-                self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
+            # Calculate log likelihood
+            log_likelihood = np.sum(
+                np.log(np.maximum(predictions, 1e-15)) * y
+                + np.log(np.maximum(1 - predictions, 1e-15)) * (1 - y)
             )
+
+            # Check for convergence based on change in log likelihood
+            if t > 1 and np.abs(log_likelihood - prev_log_likelihood) < self.threshold:
+                print(f"Optimization converged after {t} iterations.")
+                break
+
+            prev_log_likelihood = log_likelihood
+
+            m = self.beta1 * m + (1 - self.beta1) * gradient
+            v = self.beta2 * v + (1 - self.beta2) * (gradient**2)
+            m_hat = m / (1 - self.beta1**t)
+            v_hat = v / (1 - self.beta2**t)
+
+            self.weights -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
 
 
 class IWLSLogisticRegression(BaseLogisticRegression):
@@ -205,11 +220,13 @@ class IWLSLogisticRegression(BaseLogisticRegression):
     Inherits attributes from BaseLogisticRegression and adds its own specific
     for the IWLS optimization method.
     """
+
     def __init__(
-            self,
-            iterations: int = 25,
-            include_interactions: bool = False
-            ) -> None:
+        self,
+        iterations: int = 25,
+        threshold: float = 1e-5,
+        include_interactions: bool = False,
+    ) -> None:
         """
         Initializes the IWLSLogisticRegression class with specified parameters.
 
@@ -220,6 +237,7 @@ class IWLSLogisticRegression(BaseLogisticRegression):
         """
         super().__init__(include_interactions)
         self.iterations = iterations
+        self.threshold = threshold
 
     def fit(self, X: np.array, y: np.array) -> None:
         """
@@ -232,7 +250,9 @@ class IWLSLogisticRegression(BaseLogisticRegression):
         X = self._prepare_features(X)
         self.weights = np.zeros(X.shape[1])
 
-        for _ in range(self.iterations):
+        prev_log_likelihood = -1_000_000  # Initialize with a large negative value
+
+        for iter in range(self.iterations):
             model = np.dot(X, self.weights)
             predictions = sigmoid(model)
             W = np.diag(predictions * (1 - predictions))
@@ -240,9 +260,23 @@ class IWLSLogisticRegression(BaseLogisticRegression):
             try:
                 H_inv = np.linalg.inv(
                     XW @ X + np.finfo(float).eps * np.eye(X.shape[1])
-                    )  # Adding eps to prevent singularization
+                )  # Adding eps to prevent singularization
                 gradient = X.T @ (y - predictions)
                 self.weights += H_inv @ gradient
+
+                # Calculate log likelihood
+                log_likelihood = np.sum(
+                    np.log(np.maximum(predictions, 1e-15)) * y
+                    + np.log(np.maximum(1 - predictions, 1e-15)) * (1 - y)
+                )
+
+                # Check for convergence
+                if np.abs(log_likelihood - prev_log_likelihood) < self.threshold:
+                    print(f"Optimization converged after {iter + 1} iterations.")
+                    break
+
+                prev_log_likelihood = log_likelihood
+
             except np.linalg.LinAlgError:
                 break  # Loop break in case for matrix inversion problem
 
@@ -258,13 +292,15 @@ class SGDLogisticRegression(BaseLogisticRegression):
     Inherits attributes from BaseLogisticRegression and adds its own
     specific for the SGD optimization method.
     """
+
     def __init__(
-            self,
-            learning_rate: float = 0.01,
-            iterations: int = 500,
-            batch_size: int = 1,
-            include_interactions: bool = False
-            ) -> None:
+        self,
+        learning_rate: float = 0.01,
+        iterations: int = 500,
+        batch_size: int = 1,
+        threshold: float = 1e-5,
+        include_interactions: bool = False,
+    ) -> None:
         """
         Initializes the SGDLogisticRegression class with specified parameters.
 
@@ -279,6 +315,7 @@ class SGDLogisticRegression(BaseLogisticRegression):
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.batch_size = batch_size
+        self.threshold = threshold
 
     def fit(self, X: np.array, y: np.array) -> None:
         """
@@ -292,11 +329,12 @@ class SGDLogisticRegression(BaseLogisticRegression):
         self.weights = np.zeros(X.shape[1])
 
         n_samples = X.shape[0]
+        prev_log_likelihood = -1_000_000  # Initialize with a large value
 
-        for _ in range(self.iterations):
+        for iter in range(self.iterations):
             for i in range(0, n_samples, self.batch_size):
-                X_batch = X[i:i + self.batch_size]
-                y_batch = y[i:i + self.batch_size]
+                X_batch = X[i : i + self.batch_size]
+                y_batch = y[i : i + self.batch_size]
 
                 model = np.dot(X_batch, self.weights)
                 predictions = sigmoid(model)
@@ -305,3 +343,18 @@ class SGDLogisticRegression(BaseLogisticRegression):
                 gradient = -np.dot(X_batch.T, errors) / len(X_batch)
 
                 self.weights -= self.learning_rate * gradient
+
+            # Compute log-likelihood
+            model = np.dot(X, self.weights)
+            predictions = sigmoid(model)
+            log_likelihood = np.sum(
+                np.log(np.maximum(predictions, 1e-15)) * y
+                + np.log(np.maximum(1 - predictions, 1e-15)) * (1 - y)
+            )
+
+            # Check for convergence based on change in log likelihood
+            if np.abs(log_likelihood - prev_log_likelihood) < self.threshold:
+                print(f"Optimization converged after {iter + 1} iterations.")
+                break
+
+            prev_log_likelihood = log_likelihood
