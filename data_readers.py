051a26c88927f733554733d3bde19b19424cac58
Paulina Szymanek <paulinaszymanek36@gmail.com>
2024-04-02 12:52:35 +0200

    added data readers, processing and cleaned datasets

--- data_readers.py
+++ data_readers.py
@@ -1,24 +1,41 @@
 from scipy.io import arff
 import pandas as pd
-
+import numpy as np
 
 def ionosphere() -> tuple:
-    df = pd.read_csv("data/ionosphere/ionosphere.data", header=None)
+    ionosphere_data = pd.read_csv("data/ionosphere/ionosphere_clean.data", header=None,index_col=None).to_numpy()
+    X_ionosphere = ionosphere_data[:,1:-1].astype(float)
+    X_ionosphere = np.delete(X_ionosphere, np.s_[0], axis=1)
+    y_ionosphere = ionosphere_data[:,0].astype(int)
 
+<<<<<<< HEAD
+    return X_ionosphere, y_ionosphere
+=======
     df.iloc[:, -1] = df.iloc[:, -1].map({"g": 1, "b": 0})
 
     x = df.iloc[:, :-1]
     y = df.iloc[:, -1]
 
     return x, y
+>>>>>>> 595238deee6730fdb1a6246d25fbaf9afb587576
 
 
 def water_quality() -> tuple:
-    df = pd.read_csv("data/WaterQuality/water_potability.csv")
+    water_data = pd.read_csv("data/WaterQuality/water_potability_clean.csv").to_numpy()
+    water_data = water_data[~np.isnan(water_data).any(axis=1)]
+    X_water = water_data[:,:-1].astype(float)
+    y_water = water_data[:,-1].astype(int)
 
-    x = df.drop("Potability", axis=1)
-    y = df["Potability"]
+    return X_water, y_water
 
+<<<<<<< HEAD
+def heart_attack() -> tuple:
+    heart_data = pd.read_csv("data/HeartAttackAnalysis/heart.csv").to_numpy()
+    X_heart = heart_data[:, :-1].astype(int)
+    y_heart = heart_data[:, -1].astype(int)
+
+    return X_heart, y_heart
+=======
     return x, y
 
 
@@ -55,3 +72,4 @@ def female_bladder() -> tuple:
     y = bladder_df["binaryClass"].to_numpy()
 
     return X, y
+>>>>>>> 595238deee6730fdb1a6246d25fbaf9afb587576
