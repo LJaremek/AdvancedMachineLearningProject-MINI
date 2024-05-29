446f8d190e823bfc22bac974da73acefc33ca800
wpartycja <w512patrycja@gmail.com>
2024-05-29 17:14:41 +0200

    SCORE 7100 ON XGBOOST

---
+++ project_2/notebooks/tools.py
@@ -0,0 +1,45 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 1000
+TOP_RECORDS = int(TEST_SIZE*0.2)
+
+
+def calculate_money(
+        model,
+        columns_indices: list[int],
+        x_data: np.array = None,
+        y_data: np.array = None,
+        n: int = 5
+        ) -> float:
+
+    if x_data is None:
+        x_train_path = "../data/x_train.txt"
+        x_data = np.loadtxt(x_train_path, delimiter=" ")
+
+    if y_data is None:
+        y_train_path = "../data/y_train.txt"
+        y_data = np.loadtxt(y_train_path, delimiter=" ")
+
+    x_data = x_data[:, columns_indices]
+
+    money = []
+    for _ in range(n):
+        
+        x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
+
+        model.fit(x_train, y_train)
+
+        # Type TOP_RECORDS the best records
+        y_proba = model.predict_proba(x_test)[:, 1]
+        top_indices = np.argsort(y_proba)[-TOP_RECORDS:]
+        y_pred = np.zeros_like(y_test)
+        y_pred[top_indices] = 1
+
+        num_correct = np.sum((y_test == 1) & (y_pred == 1))
+        profit = (num_correct * 10) * (1_000 / TOP_RECORDS) - len(columns_indices) * 200
+
+        money.append(profit)
+
+    return sum(money)/n
